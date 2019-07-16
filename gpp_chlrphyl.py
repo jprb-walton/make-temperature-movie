4571f57778d5fed917e99ad1c400ccf6f8c88bfe
jprb-walton <jprb.walton@gmail.com>
2019-07-16 16:12:31 +0100

    Update to larger images.

--- gpp_chlrphyl.py
+++ gpp_chlrphyl.py
@@ -5,16 +5,24 @@ import matplotlib.pyplot as plt
 from iris.experimental.equalise_cubes import equalise_attributes
 from spawnCommand import SpawnCommand
 import iris.plot as iplt
+import iris.coord_categorisation as icat
+import cartopy.crs as ccrs
 
 
 def create_video():
 
     #creating the video
-    SpawnCommand("ffmpeg -i image-%04d.png TemperatureVideo1.mp4")
-    SpawnCommand('ffmpeg -i TemperatureVideo1.mp4 -filter:v "setpts=5.0*PTS" gpp.mp4')
-    print ("Deleting the unneeded images...")
-    SpawnCommand("rm -f *.png")
-    SpawnCommand("rm -f TemperatureVideo1.mp4")
+    SpawnCommand("rm -f gpp_chlrphyl_movie/gpp_chlrphyl.avi")
+    options = ("-r 5 -vcodec png -y -i " 
+             + "gpp_chlrphyl_movie/image-%04d.png -r 5 -vcodec msmpeg4v2 -qblur 0.01 -qscale 5 ")
+    SpawnCommand("ffmpeg " + options + "gpp_chlrphyl_movie/gpp_chlrphyl.avi")
+
+    #creating the video
+#    SpawnCommand("ffmpeg -i image-%04d.png TemperatureVideo1.mp4")
+#    SpawnCommand('ffmpeg -i TemperatureVideo1.mp4 -filter:v "setpts=5.0*PTS" gpp.mp4')
+#    print ("Deleting the unneeded images...")
+#    SpawnCommand("rm -f *.png")
+#    SpawnCommand("rm -f TemperatureVideo1.mp4")
     
 
 def myload(x):
@@ -46,22 +54,36 @@ def main():
     chlrphylUbound = np.amax(chlrphylcube.data)
     print(gppLbound, gppUbound)
     print(chlrphylLbound, chlrphylUbound)
+       
+    icat.add_month(gppcube, 'time')
+    months = gppcube.coord('month')
     
-
-    
-    '''
-    icat.add_year(anomaly, 'time')
-    years = anomaly.coord('year')
-    '''
     # Set the limits for the loop over years.  
     minTime = 0
     maxTime = 360
     
     fileIndex = 0
     for time in range(minTime, maxTime):
-        print time
+
+        # Avoid timesteps that are broken - ie, cause a stop with this error:
+	# ValueError: A LinearRing must have at least 3 coordinate tuple
         if time in [12, 310, 320, 347, 351, 354]: 
 	   continue
+
+        # Set up for larger image.
+        figSize = [12, 6]
+        fig = plt.figure(figsize=figSize, dpi=200)
+        rect = 0,0,200*figSize[0],200*figSize[1]
+        fig.add_axes(rect)
+        geo_axes = plt.axes(projection=ccrs.PlateCarree())
+	
+        # We need to fix the boundary of the figure (otherwise we get a black border at left & top).
+        # Cartopy removes matplotlib's axes.patch (which normally defines the boundary) and
+        # replaces it with outline_patch and background_patch.  It's the former which is causing
+        # the black border.  Get the axis object and make its outline patch invisible.
+        geo_axes.outline_patch.set_visible(False)
+        plt.margins(0,0)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
         # Contour plot the temperatures and add the coastline.
         
@@ -80,15 +102,22 @@ def main():
 
         # Extract the year value and display it (coordinates used in locating the text are
         # those of the data).
-        '''
-        plt.text(0, -60, year, horizontalalignment='center') 
-        '''
+        month = months[time].points[0]
+
+        # Display year on both sides of the display.
+        plt.text(-110, 0, month, horizontalalignment='center', 
+	         verticalalignment='top', size='large',
+	         fontdict={'family' : 'monospace'})
+        plt.text( 70, 0, month, horizontalalignment='center', 
+	         verticalalignment='top', size='large',
+		 fontdict={'family' : 'monospace'})
        
         # Now save the plot in an image file.  The files are numbered sequentially, starting
         # from 000.png; this is so that the ffmpeg command can grok them.
-        filename = "image2-%04d.png" % fileIndex
+        filename = "gpp_chlrphyl_movie/image-%04d.png" % fileIndex
 	fileIndex += 1
-        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+#        plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+        plt.savefig(filename, dpi=200)
         
         # Discard the figure (otherwise the text will be overwritten
         # by the next iteration).
